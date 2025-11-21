e4f4182dfea0d9704d33b050cf5cfa4aeaa09039
NagiKosuge <aa122072@gs.gsc.aoyama.ac.jp>
2025-11-21 12:15:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var map = new maplibregl.Map({
     container: 'map',
-    // スタイルをOpenStreetMapベースに変更します
-    style: 'https://tiles.stadiamaps.com/styles/osm_bright.json',
+    // 国土地理院の正しいURL（.geo.jpが入っています）
+    style: 'https://gsi-dev.optgeo.geo.jp/mapbox-style/std.json',
     center: [137.6625, 36.5658],
     zoom: 14, 
     
